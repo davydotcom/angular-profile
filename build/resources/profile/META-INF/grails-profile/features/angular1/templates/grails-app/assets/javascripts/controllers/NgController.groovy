79aa7f4400553ff1c292f1ccd94a5ff342304787
James Kleeh <james.kleeh@gmail.com>
2015-11-11 12:59:21 -0500

    Moved angular1 feature into the main profile Added asset pipeline feature

--- build/resources/profile/META-INF/grails-profile/features/angular1/templates/grails-app/assets/javascripts/controllers/NgController.groovy
+++
@@ -1,6 +0,0 @@
-angular.module("${packageName}")
-    .controller("${className}", ${className});
-
-function ${className}() {
-    var ctrl = this;
-}
---
+++ commands/create-ng-controller.groovy
@@ -0,0 +1,15 @@
+import grails.util.*
+
+description( "Creates an Angular controller" ) {
+    usage "grails create-ng-controller [NAME]"
+    argument name:'Controller Name', description:"The name of the Angular controller to create", required:true
+    flag name:'force', description:"Whether to overwrite existing files"
+}
+
+def model = model(args[0])
+boolean overwrite = flag('force')
+
+render template: template('NgController.groovy'),
+       destination: file("grails-app/assets/javascripts/${model.packagePath}/controllers/${model.simpleName}Controller.js"),
+       model: model,
+       overwrite: overwrite
